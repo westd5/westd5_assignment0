59ebc3f33acbb4fd0827f7294438d59499f0fc9f
Administrator <Administrator@DESKTOP-099UQRG>
2023-07-30 01:29:43 -0400

    Initial commit of spring boot project

---
+++ src/main/java/com/westd5_assignment0/Westd5Assignment0Application.java
@@ -0,0 +1,13 @@
+package com.westd5_assignment0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Westd5Assignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Westd5Assignment0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/westd5_assignment0/westd5_assignment0/Westd5Assignment0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westd5_assignment0.westd5_assignment0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Westd5Assignment0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
