f2823e93eedba6fe8a27a0e8fdfefbc29201773c
westd5 <westd5@ucmail.uc.edu>
2023-07-30 02:06:30 -0400

    Added javadoc & default end point

--- src/main/java/com/westd5_assignment0/PlantPlacesController.java
+++ src/main/java/com/westd5_assignment0/PlantPlacesController.java
@@ -6,8 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PlantPlacesController {
 	
+	/**
+	 * Handle the /start end point
+	 * @return
+	 */
 	@RequestMapping("/start")
 	public String start() {
+		return "start";
+	}
+	
+	@RequestMapping("/index")
+	public String index() {
 		return "start";
 	}
 	
